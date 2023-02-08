26a8d35ed596740ed5b178b59bd44537a456ae90
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-08 03:41:40 +0000

    2023年02月08日 - 01

--- server.js
+++ server.js
@@ -10,49 +10,31 @@ const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  console.log(hash);
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-    console.log(res);
-  });
-});
-
+//bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+//  console.log(hash);
+//  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+//    console.log(res);
+//  });
+//});
 //END_ASYNC
 
 //START_SYNC
-var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-var result = bcrypt.compareSync(myPlaintextPassword, hash);
-console.log(result);
-
+//var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+//var result = bcrypt.compareSync(myPlaintextPassword, hash);
+//console.log(result);
 //END_SYNC
 
+let str = '192.168.1.3';
 
+let hash1 = bcrypt.hashSync(str, 12);
+let hash2 = bcrypt.hashSync(str, 12);
+let hash3 = bcrypt.hashSync(str, 12);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(`hash1 : ${hash1}`);
+console.log(`compare1 : ${bcrypt.compareSync(str, hash1)}`);
+console.log(`hash2 : ${hash2}`);
+console.log(`compare2 : ${bcrypt.compareSync(str, hash2)}`);
+console.log(`hash3 : ${hash3}`);
+console.log(`compare3 : ${bcrypt.compareSync(str, hash3)}`);
 
 app.listen(process.env.PORT || 3000, () => {});
